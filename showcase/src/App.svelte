f2c6434a93c84bad4a0b9029af3af7b3765b1f54
Michael Mezzina <michaelmez39@gmail.com>
2022-07-27 14:33:20 -0400

    inital design of website, need to add pictures and content

---
+++ showcase/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Navigator from "./Navigator.svelte";
+	import Footer from "./Footer.svelte"
+	import Content from "./Content.svelte"
+	let name = 'world';
+</script>
+<style>
+	#app {
+		display: grid;
+		grid-template-columns: 1fr repeat(8, 2fr) 1fr;
+		grid-template-rows: 80px 150px 1fr 50px;
+		background-color: #003464;
+	}
+	h1 {
+		grid-column: 3/6;
+		font-family: 'Orbitron', sans-serif;
+		color: white;
+	}
+</style>
+<div id="app">
+
+	<Navigator />
+	<h1>
+		Not Just Another Showcase
+	</h1>
+	<Content />
+	<Footer />
+</div>
