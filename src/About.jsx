9fc2f671b7b1932295aa5e6f2b065e483b81cecc
Michael Mezzina <michaelmez39@gmail.com>
2022-07-28 11:17:51 -0400

    Accidently deleted the src directory...

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return <div>
+        <div>
+            <a href="https://www.flaticon.com/free-icons/portfolio" title="portfolio icons">Portfolio icons created by Smashicons - Flaticon</a>
+            <br />
+            <a href="https://www.flaticon.com/free-icons/recruitment" title="recruitment icons">Recruitment icons created by Freepik - Flaticon</a>
+            <br />
+            <a href="https://www.flaticon.com/free-icons/homepage" title="homepage icons">Homepage icons created by Andrean Prabowo - Flaticon</a>
+            <br />
+            <a href="https://www.flaticon.com/free-icons/thought" title="thought icons">Thought icons created by Freepik - Flaticon</a>
+        </div>
+    </div>
+}
